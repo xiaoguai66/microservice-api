b31429ecd245501ade9e5cd0cfd117c5bd21fef4
gx <835669138@qq.com>
2022-06-08 15:53:45 +0800

    f

--- src/ServiceA/CalculatorServiceInterface.php
+++ src/ServiceA/CalculatorServiceInterface.php
@@ -4,5 +4,11 @@ namespace Xiaoguai\Microservice\ServiceA;
 
 interface CalculatorServiceInterface
 {
+    /**
+     * 两数相加
+     * @param int $a
+     * @param int $b
+     * @return int
+     */
     public function add(int $a, int $b): int;
 }
